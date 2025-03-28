94ffb50eb609fa39fb9dda0811d7958f38f355a8
skyrabbit4 <111689974+skyrabbit4@users.noreply.github.com>
2025-03-29 03:55:51 +0530

    Convert TodoApi from submodule to regular folder with updated TodoItem

---
+++ TodoApi/TodoApi/Model/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TodoApi.Model
+{
+	public class TodoItem
+	{
+		public int Id { get; set; }
+		public string? Title { get; set; }//kkl
+		public bool IsComplete { get; set; }
+	}
+}
+
