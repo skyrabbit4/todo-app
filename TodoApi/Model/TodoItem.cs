0acf6b92f157f2983a0487929a481439c1ad076f
skyrabbit4 <111689974+skyrabbit4@users.noreply.github.com>
2025-03-29 02:42:47 +0530

    commet

--- TodoApi/Model/TodoItem.cs
+++ TodoApi/Model/TodoItem.cs
@@ -4,7 +4,7 @@ namespace TodoApi.Model
 	public class TodoItem
 	{
 		public int Id { get; set; }
-		public string? Title { get; set; }
+		public string? Title { get; set; }//use to store id
 		public bool IsComplete { get; set; }
 	}
 }
