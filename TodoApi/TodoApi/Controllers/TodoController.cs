c67edd97824ff9f1df861803dac7c12a8ddae5a2
skyrabbit4 <111689974+skyrabbit4@users.noreply.github.com>
2025-03-29 04:37:47 +0530

    Added get todo by Id

--- TodoApi/TodoApi/Controllers/TodoController.cs
+++ TodoApi/TodoApi/Controllers/TodoController.cs
@@ -6,13 +6,24 @@ namespace TodoApi.Controllers
 {
 	[ApiController]
 	[Route("api/[controller")]
-	public class TodoController:ControllerBase
+	public class TodoController : ControllerBase
 	{
 		private static List<TodoItem> Todos = new List<TodoItem>();
 		[HttpGet]
-		public ActionResult<IEnumerable<TodoItem>>GetTodos()
+		public ActionResult<IEnumerable<TodoItem>> GetTodos()
 		{
 			return Ok(Todos);
+		}
+
+		[HttpGet("{id}")]
+		public ActionResult<TodoItem>GetTodo(int id)
+		{
+			var todo = Todos.FirstOrDefault(t => t.Id == id);
+			if(todo==null)
+			{
+				return NotFound();
+			}
+			return Ok(todo);
 		}
 
 		
