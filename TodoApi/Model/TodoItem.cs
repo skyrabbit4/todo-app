fbae0e9c0e06f9ee9bd1c39ea4701e366097e675
skyrabbit4 <111689974+skyrabbit4@users.noreply.github.com>
2025-03-29 02:35:41 +0530

    files

---
+++ TodoApi/Model/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TodoApi.Model
+{
+	public class TodoItem
+	{
+		public int Id { get; set; }
+		public string? Title { get; set; }
+		public bool IsComplete { get; set; }
+	}
+}
+
