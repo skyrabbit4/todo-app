1e51c948ce3934548cdf098bf5abf89a961c94fa
skyrabbit4 <111689974+skyrabbit4@users.noreply.github.com>
2025-03-29 04:18:47 +0530

    added get controller for todo

---
+++ TodoApi/TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Model;
+
+namespace TodoApi.Controllers
+{
+	[ApiController]
+	[Route("api/[controller")]
+	public class TodoController:ControllerBase
+	{
+		private static List<TodoItem> Todos = new List<TodoItem>();
+		[HttpGet]
+		public ActionResult<IEnumerable<TodoItem>>GetTodos()
+		{
+			return Ok(Todos);
+		}
+
+		
+	}
+}
+
