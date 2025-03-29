8fa98c2924f2530b9cb5264e897124438ec5522a
skyrabbit4 <111689974+skyrabbit4@users.noreply.github.com>
2025-03-29 21:36:18 +0530

    added form and list

---
+++ frontend/src/app/todo-form/todo-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-form',
+  imports: [],
+  templateUrl: './todo-form.component.html',
+  styleUrl: './todo-form.component.css'
+})
+export class TodoFormComponent {
+
+}
