23ab3d4716f8e10db72bee0e5c1b62ed939e50cb
skyrabbit4 <111689974+skyrabbit4@users.noreply.github.com>
2025-03-29 20:50:19 +0530

    added todo service

--- frontend/src/app/todo.service.ts
+++ frontend/src/app/todo.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface TodoItem {
+  id: number;
+  title: string;
+  isComplete: boolean;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  private apiUrl = 'http://localhost:5048/swagger/index.html'; // Adjust to your actual API port
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getTodos(): Observable<TodoItem[]> {
+    return this.http.get<TodoItem[]>(`${this.apiUrl}/todo`);
+  }
+
+  createTodo(todo: Omit<TodoItem, 'id'>): Observable<TodoItem> {
+    return this.http.post<TodoItem>(`${this.apiUrl}/todo`, todo);
+  }
+
+  updateTodo(todo: TodoItem): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/todo/${todo.id}`, todo);
+  }
+
+  deleteTodo(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/todo/${id}`);
+  }
 }
